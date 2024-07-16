d4a5fbcbd88a4f4354205db0b19c03f672648927
kunsect <kunsect@gmail.com>
2024-07-16 16:45:21 +0800

    init

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { WalletManager } from './wallet'
+import 'dotenv/config'
+
+const { START_PATH_INDEX, MAX_PATH_INDEX } = process.env
+
+let pathIndex: number = Number(START_PATH_INDEX)
+const maxPathIndex: number = Number(MAX_PATH_INDEX)
+
+for (let index = pathIndex; index <= maxPathIndex; index++) {
+  const wallet = new WalletManager(index)
+  const taproot = wallet.taprootWallet
+  const p2wpkh = wallet.p2wpkhWallet
+
+  console.log(`---- Account ${index + 1} ----`)
+  console.log(`[Taproot]\naddress: ${taproot.address}\nprivate key: ${taproot.privateKey}\n`)
+  console.log(`[P2WPKH]\naddress: ${p2wpkh.address}\nprivate key: ${p2wpkh.privateKey}\n\n`)
+}
---
+++ wallet.ts
@@ -0,0 +1,82 @@
+import * as bitcoin from 'bitcoinjs-lib'
+import * as bip39 from 'bip39'
+import * as ecc from 'tiny-secp256k1'
+import BIP32Factory, { BIP32Interface } from 'bip32'
+import 'dotenv/config'
+
+const MNEMONIC = process.env.MNEMONIC!
+
+bitcoin.initEccLib(ecc)
+const bip32 = BIP32Factory(ecc)
+
+export interface ITaprootWallet {
+  address: string
+  path: string
+  childNode: BIP32Interface
+  payment: bitcoin.Payment
+  internalPubkey: Buffer
+  tweakedChildNode: bitcoin.Signer
+  privateKey: string | undefined
+}
+
+export interface IP2WPKHWallet {
+  address: string
+  path: string
+  childNode: BIP32Interface
+  payment: bitcoin.Payment
+  privateKey: string | undefined
+}
+
+export class WalletManager {
+  pathIndex: number
+
+  taprootWallet: ITaprootWallet
+  p2wpkhWallet: IP2WPKHWallet
+
+  constructor(pathIndex: number) {
+    this.pathIndex = pathIndex
+
+    this.taprootWallet = this.getTaprootWallet()
+    this.p2wpkhWallet = this.getP2WPKHWallet()
+  }
+
+  private getTaprootWallet(): ITaprootWallet {
+    const seed = bip39.mnemonicToSeedSync(MNEMONIC)
+    const rootKey = bip32.fromSeed(seed)
+
+    const path = `m/86'/0'/${this.pathIndex}'/0/0`
+    const childNode = rootKey.derivePath(path)
+    const internalPubkey = childNode.publicKey.subarray(1, 33)
+
+    const payment = bitcoin.payments.p2tr({ internalPubkey })
+
+    const tweakedChildNode = childNode.tweak(bitcoin.crypto.taggedHash('TapTweak', internalPubkey))
+
+    return {
+      address: payment.address!,
+      path,
+      payment,
+      childNode,
+      internalPubkey,
+      tweakedChildNode,
+      privateKey: childNode.privateKey?.toString('hex')
+    }
+  }
+
+  private getP2WPKHWallet(): IP2WPKHWallet {
+    const seed = bip39.mnemonicToSeedSync(MNEMONIC)
+    const rootKey = bip32.fromSeed(seed)
+
+    const path = `m/84'/0'/${this.pathIndex}'/0/0`
+    const childNode = rootKey.derivePath(path)
+    const { address } = bitcoin.payments.p2wpkh({ pubkey: childNode.publicKey })
+
+    return {
+      address: address!,
+      path,
+      childNode,
+      payment: bitcoin.payments.p2wpkh({ pubkey: childNode.publicKey }),
+      privateKey: childNode.privateKey?.toString('hex')
+    }
+  }
+}
